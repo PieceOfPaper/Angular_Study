7b2447410b542b47535f7053e125ce21ea02a82b
이성희 <>
2021-09-21 14:42:12 +0900

    19강 https://lts0606.tistory.com/375?category=775312

--- thirdStudy/src/app/app.component.ts
+++ thirdStudy/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFirestoreModule, AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'thirdStudy';
+
+  private DataBase : AngularFirestore;
+  private itemsCollection: AngularFirestoreCollection<any> | undefined;
+
+  constructor(db : AngularFirestore) {   //모듈에서 만들어진 파이어 베이스 접속관련 객체
+    this.DataBase = db;
+    this.getItem('board').subscribe((res)=>{  //board 컬렉션에 대해서 구독행위 시작
+      console.log(res);
+    });
+  } 
+
+  getItem(db_name : string){
+    this.itemsCollection = this.DataBase.collection<any>(db_name, (ref) =>ref);  //컬렉션에 접속    
+    return this.itemsCollection.valueChanges();  //리턴
+  }
 }
--- thirdStudy/src/app/app.module.ts
+++ thirdStudy/src/app/app.module.ts
@@ -9,6 +9,22 @@ import { BoardComponent } from './board/board.component';
 import { RouterModule, Routes} from '@angular/router';
 import { AuthGuard } from './auth.guard';
 
+import { AngularFireModule} from '@angular/fire/compat'
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
+const fireEnvironment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDcdtBBp1RgnMN2avYAZEV9pJicmo0SrxI",
+    authDomain: "angular-study-965ed.firebaseapp.com",
+    projectId: "angular-study-965ed",
+    storageBucket: "angular-study-965ed.appspot.com",
+    messagingSenderId: "492546308019",
+    appId: "1:492546308019:web:5694f3ff68a245bb96fdc6",
+    measurementId: "G-M4H357EE5H"
+  }
+}
+
 const router : Routes = [  //라우팅
   {path : 'login' , component : LoginComponent},  
   {path : 'board' , component : BoardComponent, canActivate:[AuthGuard]},
@@ -23,8 +39,10 @@ const router : Routes = [  //라우팅
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(router,{enableTracing: false, useHash: true}),
+    RouterModule.forRoot(router,{enableTracing: false}),
     FormsModule,
+    AngularFireModule.initializeApp(fireEnvironment.firebase, '/'),
+    AngularFirestoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
