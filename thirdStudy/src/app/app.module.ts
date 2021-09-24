630fd40c5a6c33e985886c22760ca1f870f20d10
이성희 <>
2021-09-24 12:43:15 +0900

    26강 https://lts0606.tistory.com/401?category=775312

--- thirdStudy/src/app/app.module.ts
+++ thirdStudy/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms';
+import { FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -8,9 +8,13 @@ import { BoardComponent } from './board/board.component';
 
 import { RouterModule, Routes} from '@angular/router';
 import { AuthGuard } from './auth.guard';
+import { AskService } from './ask.service';
 
 import { AngularFireModule} from '@angular/fire/compat'
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';  //저장소 모듈
+import { BUCKET } from '@angular/fire/compat/storage';  //저장소 관련 버킷
 
 const fireEnvironment = {
   production: true,
@@ -42,9 +46,12 @@ const router : Routes = [  //라우팅
     RouterModule.forRoot(router,{enableTracing: false}),
     FormsModule,
     AngularFireModule.initializeApp(fireEnvironment.firebase, '/'),
-    AngularFirestoreModule,
+    AngularFirestoreModule,  //파이어베이스 데이터베이스와 관련된 모듈 사용
+    AngularFireStorageModule  //파일 저장소 관련된 객체
   ],
-  providers: [],
+  providers: [
+    AskService,
+    { provide: BUCKET, useValue: fireEnvironment.firebase.storageBucket }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- thirdStudy/src/app/board/board.component.ts
+++ thirdStudy/src/app/board/board.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+
 
 @Component({
   selector: 'app-board',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoardComponent implements OnInit {
 
-  constructor() { }
+  private file: any | undefined;
+  private path = "good";  //경로
+
+  constructor(private storage: AngularFireStorage) { }
 
   ngOnInit(): void {
+    
+  }
+
+  uploadFile(event : any) {
+    this.file = event.target.files[0];
+  }
+
+  send(){
+    const ref = this.storage.upload(this.path + this.file?.name, this.file);
+    console.log(ref);
   }
 
 }
