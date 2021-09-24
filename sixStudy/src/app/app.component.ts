4d1a0f19ecb6330761d1fa2504f195c6fbe7eea6
이성희 <>
2021-09-24 14:55:27 +0900

    33강 https://lts0606.tistory.com/418?category=775312

--- sixStudy/src/app/app.component.ts
+++ sixStudy/src/app/app.component.ts
@@ -9,4 +9,27 @@ import { my_check } from './my-check.directive';
 })
 export class AppComponent {
   title = new FormControl('', my_check()); 
+  
+  private canElementMove = false;  //마우스의 상태를 나타내는 변수 입니다.
+
+  position = {
+    left : '0px',
+    top : '0px',
+    width : '200px',
+    height : '200px',
+    border : '1px solid gray',
+    position : 'absolute'
+  };
+
+  set mouseStatus(arg : any){  //마우스의 상태가 변화됨을 적용하는 함수 입니다.
+    this.canElementMove = arg;
+  }
+
+  getEvent(event : any) : void{
+    if(this.canElementMove){
+      //console.log(event)
+      this.position.left = event.clientX - 100  + 'px';
+      this.position.top = event.clientY - 100   + 'px';
+    }
+  }
 }
